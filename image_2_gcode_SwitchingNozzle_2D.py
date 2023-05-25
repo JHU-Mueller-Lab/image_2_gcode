c1d73a2d49401a10c27bec194b5af5dc11bf5b73
Sarah Propst <116668788+spropst4@users.noreply.github.com>
2023-05-25 10:19:12 -0400

    Update image_2_gcode_SwitchingNozzle_2D.py

--- image_2_gcode_SwitchingNozzle_2D.py
+++ image_2_gcode_SwitchingNozzle_2D.py
@@ -5,6 +5,9 @@ Purpose: convert image to gcode print path
 """
 import cv2
 import numpy as np
+def rgb_to_hex(b, g, r):
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
 
 '''This function can be used to generate 2 Material prints'''
 def image_2_gcode_2Materials(image_name, y_dist, offset, color1, color2,other_color_50_50_split, other_color, color1_ON, color1_OFF, color2_ON, color2_OFF, gcode_simulate):
@@ -83,13 +86,20 @@ def image_2_gcode_2Materials(image_name, y_dist, offset, color1, color2,other_co
     return gcode_list
 
 '''This function can be used to generate 2+ Material prints'''
-def image_2_gcode_2plusMaterials(image_name, y_dist, offset, color_list, other_color_50_50_split, other_color, color_ON_list, color_OFF_list, gcode_simulate, gcode_simulate_color):
+def image_2_gcode_2plusMaterials(image_name, y_dist, offset, color_list, other_color_50_50_split, other_color, color_ON_list, color_OFF_list, gcode_simulate, gcode_simulate_color, convert_to_hex):
     if gcode_simulate == True:
         gcode_color1 = "G0 X"
     else:
         gcode_color1 = "G1 X"
 
     img = cv2.imread(image_name, 0)
+    if convert_to_hex == True:
+        img = cv2.imread(image_name)
+
+    img = cv2.flip(img, 1)
+    # cv2.imshow('flipped', img)
+    # cv2.waitKey(0)
+    #
 
     dist = ''
     prev_pixel = ''
@@ -98,19 +108,22 @@ def image_2_gcode_2plusMaterials(image_name, y_dist, offset, color_list, other_c
     gcode = ''
     gcode_list = []
     for i in range(len(img)):  # number of rows of pixels  (image height)
+
         if (i + 1) % 2 == 0:  # even rows:
             img[i] = np.flip(img[i])  # reverse order of pixel
             dist_sign = '-'  # reverse x-direction of print
         else:  # odd rows:
             dist_sign = ''
 
-
-
         for j in range(len(img[i])):  # number of pixels in a row (image width)
             pixel = img[i][j]
 
+            if convert_to_hex == True:
+                pixel = rgb_to_hex(pixel[0], pixel[1], pixel[2])
+                #pix_hex_list.append(pix_hex)
+
             if pixel not in color_list:
-                if other_color_50_50_split == True: # True: pixel becomes color that it is closest to; False: pixel color is chosen as you specify below
+                if other_color_50_50_split == True and convert_to_hex == False: # True: pixel becomes color that it is closest to; False: pixel color is chosen as you specify below
                     diff_pixel = []
                     for color in range(len(color_list)):
                         diff_pixel.append(abs(pixel - color))
@@ -268,8 +281,8 @@ NOTES:
     
 '''
 ############################################### 2 Colors function ################################
-image_name = 'Heart50x50.png'
-gcode_export = 'Heart50x50_2Material.txt'
+image_name = 'blue_jay_75x75pics.png'
+gcode_export = 'blue_jay_75x75pics_2Material.txt'
 
 y_dist = 1  # width of filament
 offset = 0
@@ -282,8 +295,8 @@ color1 = black
 color2 = white
 
 # what color do you want pixels that aren't black or white?
-other_color_50_50_split = False # True: pixel becomes color that it is closest to; False: pixel color is chosen as you specify below
-other_color = black
+other_color_50_50_split = True # True: pixel becomes color that it is closest to; False: pixel color is chosen as you specify below
+other_color = white
 
 
 color1_ON = 'Black ON'
@@ -299,26 +312,39 @@ with open(gcode_export, 'w') as f:
         f.write(elem + '\n')
 
 ############################################### 2+ Colors function ################################
-image_name = 'test_smiley_v2.png'
-gcode_export = 'test_smiley_v2_3Material.txt'
+image_name = 'blue_jay_75x75pixs_3colors.png'
+gcode_export = 'blue_jay_75x75pixs_3colors_3Material.txt'
+
+img = cv2.imread(image_name)
+pix_hex_list = []
+
 
 y_dist = 1  # width of filament
 offset = 0
 
-black = 0
-white = 255
-gray = 191
+convert_to_hex = True
+if convert_to_hex == True:
+    ## HEX:
+    black = '#000000'
+    white = '#ffffff'
+    blue =  '#68ace5'
+
+else:
+    ## Grayscale:
+    black = 0
+    white = 255
+    gray = 150
 
 color1 = black
 color2 = white
-color3 = gray
+color3 = blue
 
 # what color do you want pixels that aren't black or white?
-other_color_50_50_split = False # True: pixel becomes color that it is closest to; False: pixel color is chosen as you specify below
-other_color = gray
+other_color_50_50_split = True # ONly works in grayscale mode; True: pixel becomes color that it is closest to; False: pixel color is chosen as you specify below
+other_color = blue
 
 gcode_simulate = True  # If True: writes black (color1) pixels as 'G0' moves
-gcode_simulate_color = gray
+gcode_simulate_color = color1
 
 
 color1_ON = 'Black ON'
@@ -328,11 +354,13 @@ color2_OFF = 'White OFF'
 color3_ON = 'Gray ON'
 color3_OFF = 'Gray OFF'
 
-color_list = [black, white, gray]
+color_list = [color1, color2, color3]
 color_ON_list = [color1_ON, color2_ON, color3_ON]
 color_OFF_list = [color1_OFF, color2_OFF, color3_OFF]
 
-gcode_list_2plus_colors = image_2_gcode_2plusMaterials(image_name, y_dist, offset, color_list, other_color_50_50_split, other_color, color_ON_list, color_OFF_list, gcode_simulate, gcode_simulate_color)
+gcode_list_2plus_colors = image_2_gcode_2plusMaterials(image_name, y_dist, offset, color_list, other_color_50_50_split,
+                                                       other_color, color_ON_list, color_OFF_list, gcode_simulate,
+                                                       gcode_simulate_color, convert_to_hex)
 with open(gcode_export, 'w') as f:
     f.write('G91\r')
     for elem in gcode_list_2plus_colors:
